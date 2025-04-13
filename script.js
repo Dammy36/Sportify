e26e60f458a4137f8e835f6ef7a83312881b8e8c
Dammie <osedee101@gmail.com>
2025-04-13 10:08:31 +0100

    Removed DOMContentLoaded from JS

--- script.js
+++ script.js
@@ -3,20 +3,3 @@ const items = document.getElementById("account-list");
 list.addEventListener("click", function () {
   items.classList.toggle("account-list-show");
 });
-
-document.addEventListener("DOMContentLoaded", function () {
-  const header = document.querySelector(".grid-item-1-container");
-
-  const observer = new IntersectionObserver(
-    function (entries) {
-      if (entries[0].isIntersecting) {
-        header.classList.remove("shadow");
-      } else {
-        header.classList.add("shadow");
-      }
-    },
-    { threshold: [0] }
-  );
-
-  observer();
-});
